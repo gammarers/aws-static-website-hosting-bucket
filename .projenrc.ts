e305f3acd7115196c08419001aeefc1821eefdae
yicr <yicr@users.noreply.github.com>
2024-09-27 18:36:31 +0900

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,27 @@
+import { awscdk, javascript } from 'projen';
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'yicr',
+  authorAddress: 'yicr@users.noreply.github.com',
+  cdkVersion: '2.80.0',
+  defaultReleaseBranch: 'main',
+  typescriptVersion: '5.5.x',
+  jsiiVersion: '5.5.x',
+  name: '@gammarers/aws-static-website-hosting-bucket',
+  projenrcTs: true,
+  repositoryUrl: 'https://github.com/gammarers/aws-static-website-hosting-bucket.git',
+  releaseToNpm: false,
+  npmAccess: javascript.NpmAccess.PUBLIC,
+  minNodeVersion: '18.0.0',
+  workflowNodeVersion: '22.4.x',
+  depsUpgradeOptions: {
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      schedule: javascript.UpgradeDependenciesSchedule.expressions(['3 17 * * 0']), // every sunday (JST/MON:02:00)
+    },
+  },
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['yicr'],
+  },
+});
+project.synth();
