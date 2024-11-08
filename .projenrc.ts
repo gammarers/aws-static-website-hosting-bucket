80ce24be4fa73c4842647aeb3ea2098507db3b1f
yicr <yicr@users.noreply.github.com>
2024-11-08 10:58:30 +0900

    chore: upgarade main schedule changed #4

--- .projenrc.ts
+++ .projenrc.ts
@@ -20,8 +20,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   depsUpgradeOptions: {
     workflowOptions: {
       labels: ['auto-approve', 'auto-merge'],
-      //schedule: javascript.UpgradeDependenciesSchedule.expressions(['3 17 * * 0']), // every sunday (JST/MON:02:00)
-      schedule: javascript.UpgradeDependenciesSchedule.expressions(['30 6 * * *']),
+      schedule: javascript.UpgradeDependenciesSchedule.expressions(['3 17 * * 0']),
     },
   },
   autoApproveOptions: {
